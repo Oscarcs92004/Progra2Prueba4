caa80ecb1d590dc6768916ce2ba652936f54455d
Oscarcs92004 <oscarcs92004@gmail.com>
2026-08-13 07:05:13 -0600

    agrego nuevas funciones a Gestor Pedidos

--- Progra2Prueba4/src/GestorPedidos.java
+++ Progra2Prueba4/src/GestorPedidos.java
@@ -25,7 +25,33 @@ public class GestorPedidos {
         cantidad++;
     }
     
-    public void buscarPedido(Pedido dato) throws PedidoNoEncontradoException {
-        
+    public Pedido buscarPorId(int id) throws PedidoNoEncontradoException {
+        for(int i = 0; i < pedidos.length;i++){
+            if(pedidos[i].getId() == id){
+                return pedidos[i];
+            }
+        }
+        throw new PedidoNoEncontradoException(id);
     }
+    
+    public void cambiarEstado(int id, EstadoPedido nuevoEstado) throws PedidoNoEncontradoException, TransicionEstadoException{
+        Pedido pedido = buscarPorId(id);
+        if(!pedido.getEstado().transicionarA(nuevoEstado)){
+            throw new TransicionEstadoException(pedido.getEstado(), nuevoEstado);
+        }
+        pedido.setEstado(nuevoEstado);
+    }
+    
+    public Pedido[] listadoPorEstado(EstadoPedido estado){
+        Pedido[] p = new Pedido[pedidos.length];
+        int contador = 0;
+        for(int i = 0; i < pedidos.length;i++){
+            if(pedidos[i].getEstado() == estado){
+                p[contador] = pedidos[i];
+                contador++;
+            }
+        }
+        return p;
+    }
+    
 }
